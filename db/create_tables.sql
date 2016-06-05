80abb4d8c0d671936bcbc56d3e9e56d995063b7d
James <james@Jamess-Air.lan>
2016-06-05 00:08:30 -0500

    Modified table to use timestamp instead of date

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,14 +1,34 @@
-CREATE TABLE IF NOT EXISTS `equity_snapshot` (
-  `snapshot_id` INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS `equity` (
+  `equity_id` INT AUTO_INCREMENT NOT NULL,
   `ticker` VARCHAR(10) NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `exchange` VARCHAR(10) NOT NULL,
-  `date` DATE NOT NULL,
+  `industry` VARCHAR(50),
+  PRIMARY KEY (`equity_id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `equity_snapshot` (
+  `snapshot_id` INT AUTO_INCREMENT NOT NULL,
+  `equity_id` INT NOT NULL,
+  `date` DATE NOT NULL ,
   `price` FLOAT(10,4) NOT NULL,
   `price_change` FLOAT(10,4) NOT NULL,
   `price_change_percent` FLOAT(10,4) NOT NULL,
-  PRIMARY KEY (`snapshot_id`)
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_bin
+  PRIMARY KEY (`snapshot_id`),
+  FOREIGN KEY (`equity_id`) REFERENCES `equity`(`equity_id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `equity_aggregate` (
+  `aggregate_id` INT AUTO_INCREMENT NOT NULL,
+  `equity_id` INT NOT NULL,
+  `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `fifty_day_moving_avg` FLOAT(10,4) NOT NULL,
+  `fifty_day_volatility_avg` FLOAT(10,4) NOT NULL,
+  `per_off_recent_high` FLOAT(10,4) NOT NULL,
+  `per_off_recent_low` FLOAT(10,4) NOT NULL,
+  PRIMARY KEY (`aggregate_id`),
+  FOREIGN KEY (`equity_id`) REFERENCES `equity`(`equity_id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_bin;
 
 CREATE VIEW fifty_day_moving_average AS
 SELECT `ticker`, AVG(`price`) AS average_price
