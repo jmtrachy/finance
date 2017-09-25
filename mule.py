48e076ba6af43ee92352fded03c0d82679e77a5c
Ubuntu <ubuntu@ip-172-30-0-100.ec2.internal>
2017-09-25 17:50:21 +0000

    Started add microscopic new features to mule

--- mule.py
+++ mule.py
@@ -6,6 +6,12 @@ from operator import attrgetter
 password = myconfig.mule_password
 network = myconfig.mule_network
 port = myconfig.mule_port
+all_equities = set()
+
+def build_ticker_set():
+   all_equities_raw = sorted(EquityDAO.get_equities(), key=attrgetter('ticker'), reverse=False)
+   for e in all_equities_raw:
+      all_equities.add(e.ticker)
 
 def get_recent_snapshots(equities):
    recent_snapshots = []
@@ -23,9 +29,11 @@ def get_recent_aggregates(equities):
 
    return aggregates 
 
+build_ticker_set()
 print('mule is connecting to ' + str(network) + ':' + str(port))
-irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-irc.connect ( ( network, port ) )
+irc = socket.socket (socket.AF_INET, socket.SOCK_STREAM )
+irc.settimeout(None)
+irc.connect((network, port))
 
 irc.send('PASS ' + password + '\r\n')
 irc.send('NICK mule\r\n')
@@ -37,13 +45,13 @@ while keep_running:
    data = irc.recv ( 1024 )
    if data.find('PING') != -1:
       irc.send('PONG ' + data.split() [ 1 ] + '\r\n')
-   if data.find('@mule help') != -1:
+   elif data.find('@mule help') != -1:
       irc.send('PRIVMSG #pynerds :I respond to stock <ticker>, dod, dow, div, drop, vol, tracked, meta and quit.\r\n')
-   if data.find('@mule quit') != -1:
+   elif data.find('@mule quit') != -1:
       irc.send('PRIVMSG #pynerds :Ok bye.\r\n')
       irc.send('QUIT\r\n')
       keep_running = False
-   if data.find('@mule stock') != -1:
+   elif data.find('@mule stock') != -1:
       tickers= data.split()
       ticker = tickers[5].upper()
       if ticker is None:
@@ -62,7 +70,7 @@ while keep_running:
             for ea in equity.aggregates:
                 irc.send('PRIVMSG #pynerds : fifty day moving avg: {0}; fifty day volatility avg: {1}; % off recent high: {2}; % off recent low: {3}\r\n'.format(ea.fifty_day_moving_avg, \
                          ea.fifty_day_volatility_avg, ea.per_off_recent_high, ea.per_off_recent_low))
-   if data.find('@mule dod') != -1:
+   elif data.find('@mule dod') != -1:
       recent_snapshots = get_recent_snapshots(EquityDAO.get_dow_equities())
       equities = sorted(recent_snapshots, key=attrgetter('dividend_yield'), reverse=True)
      
@@ -73,7 +81,7 @@ while keep_running:
          equity = EquityDAO.get_equity_by_id(stock.equity_id)
          irc.send('PRIVMSG #pynerds :{}. {} yields {:.2f}%\r\n'.format(j + 1, equity.ticker, stock.dividend_yield))
 
-   if data.find('@mule dow') != -1:
+   elif data.find('@mule dow') != -1:
       recent_snapshots = get_recent_snapshots(EquityDAO.get_dow_equities())
       recent_snapshots = sorted(recent_snapshots, key=attrgetter('price_change'), reverse=True)
 
@@ -83,7 +91,7 @@ while keep_running:
          irc.send('PRIVMSG #pynerds :{}. {} is at {}.  {:.2f}% from yesterday\r\n'.format(j, equity.ticker, snapshot.price, snapshot.price_change))
          j += 1
 
-   if data.find('@mule div') != -1:
+   elif data.find('@mule div') != -1:
       recent_snapshots = get_recent_snapshots(EquityDAO.get_equities())
       equities = sorted(recent_snapshots, key=attrgetter('dividend_yield'), reverse=True)
      
@@ -96,7 +104,7 @@ while keep_running:
             equity = EquityDAO.get_equity_by_id(stock.equity_id)
             irc.send('PRIVMSG #pynerds :{}. {} yields {:.2f}%\r\n'.format(j + 1, equity.ticker, stock.dividend_yield))
 
-   if data.find('@mule drop') != -1:
+   elif data.find('@mule drop') != -1:
       recent_aggregates = get_recent_aggregates(EquityDAO.get_equities())
       aggregates = sorted(recent_aggregates, key=attrgetter('per_off_recent_high'), reverse=True)
 
@@ -107,7 +115,7 @@ while keep_running:
          equity = EquityDAO.get_equity_by_id(aggregate.equity_id)
          irc.send('PRIVMSG #pynerds :{}. {} is off {:.2f}% from its recent high\r\n'.format(j + 1, equity.ticker, aggregate.per_off_recent_high))
 
-   if data.find('@mule vol') != -1:
+   elif data.find('@mule vol') != -1:
       recent_aggregates = get_recent_aggregates(EquityDAO.get_equities())
       aggregates = sorted(recent_aggregates, key=attrgetter('fifty_day_volatility_avg'), reverse=True)
 
@@ -118,7 +126,7 @@ while keep_running:
          equity = EquityDAO.get_equity_by_id(aggregate.equity_id)
          irc.send('PRIVMSG #pynerds :{}. {} on average moves {:.2f}% per day\r\n'.format(j + 1, equity.ticker, aggregate.fifty_day_volatility_avg))
 
-   if data.find('@mule tracked') != -1:
+   elif data.find('@mule tracked') != -1:
       equities = sorted(EquityDAO.get_equities(), key=attrgetter('ticker'), reverse=False)
 
       irc.send('PRIVMSG #pynerds :All currently tracked stocks\r\n')
@@ -130,7 +138,7 @@ while keep_running:
       equity_list = equity_list[2:]
       irc.send('PRIVMSG #pynerds :{}\r\n'.format(equity_list)) 
    
-   if data.find('@mule meta') != -1:
+   elif data.find('@mule meta') != -1:
       equities = EquityDAO.get_equity_meta()
 
       irc.send('PRIVMSG #pynerds :Meta on all currently traded stocks.  Format: ticker (number of days tracked)\r\n')
@@ -141,3 +149,11 @@ while keep_running:
 
       equity_list = equity_list[2:]
       irc.send('PRIVMSG #pynerds :{}\r\n'.format(equity_list)) 
+   else:
+      if data is not None and data != '':
+         all_words = data.split(' ')
+         for word in all_words:
+            word_stripped = word.strip(',').strip('.').upper()
+            if word_stripped in all_equities:
+               equity = EquityDAO.get_equity_with_most_recent_data(word_stripped, 1, 1)
+               irc.send('PRIVMSG #pynerds :{} last closed at {}\r\n'.format(equity.ticker, equity.snapshots[0].price))
